219233bc5eb78e239681267a08fa8a8d13595382
Ro-bu <robin.raid1@gmail.com>
2022-05-15 00:00:20 +0300

    added error handling displaying

--- src/populateUi.js
+++ src/populateUi.js
@@ -1,14 +1,22 @@
 import {fetchWeather} from "./fetchweather.js";
 
 async function populateUi (location) {
-    let weatherData = await fetchWeather(location);
-    document.querySelector("#location-name").textContent = weatherData.name;
-    document.querySelector("#temperature").textContent = Math.floor(weatherData.main.temp);
-    document.querySelector("#description").textContent = weatherData.weather[0].description.charAt(0).toUpperCase() + weatherData.weather[0].description.slice(1);
-    document.querySelector("#feels-like").textContent = Math.floor(weatherData.main.feels_like);
-    document.querySelector("#wind").textContent = `WIND: ${Math.floor(weatherData.wind.speed*10)/10} m/s`;
-    document.querySelector("#pressure").textContent = `PRESSURE: ${weatherData.main.pressure}`;
-    document.querySelector("#humidity").textContent = `HUMIDITY: ${weatherData.main.humidity}%`;
+    try{
+        document.querySelector("#error-message").classList.remove("visible");
+        let weatherData = await fetchWeather(location);
+        document.querySelector("#location-name").textContent = weatherData.name + ", " + weatherData.sys.country;
+        document.querySelector("#temperature").textContent = Math.floor(weatherData.main.temp);
+        document.querySelector("#description").textContent = weatherData.weather[0].description.charAt(0).toUpperCase() + weatherData.weather[0].description.slice(1);
+        document.querySelector("#feels-like").textContent = Math.floor(weatherData.main.feels_like);
+        document.querySelector("#wind").textContent = `WIND: ${Math.floor(weatherData.wind.speed*10)/10} m/s`;
+        document.querySelector("#pressure").textContent = `PRESSURE: ${weatherData.main.pressure}`;
+        document.querySelector("#humidity").textContent = `HUMIDITY: ${weatherData.main.humidity}%`;
+    } catch(error) {
+        console.log(error);
+        document.querySelector("#location-name").textContent = "Oops...";
+        document.querySelector("#error-message").classList.add("visible");
+    }
+
 };
 
 export {populateUi};
